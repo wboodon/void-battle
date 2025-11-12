f8c9970c12fc8bca67740f9d5d6f690eee7d2b3a
wboodon <38161399+wboodon@users.noreply.github.com>
2025-11-12 12:50:44 -0500

    More small changes

--- images.g.ts
+++ images.g.ts
@@ -939,63 +939,6 @@ c c c c c c c c b b b b b c c c
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `];
-            case "witchPushForward":
-            case "anim20":return [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . c c . . . . . . . . 
-. . . . . . c 5 c . . . . . . . 
-. . . . c c c 5 5 c c c . . . . 
-. . c c c c 5 5 5 5 c b c c . . 
-. c b b 5 b 5 5 5 5 b 5 b b c . 
-. c b 5 5 b b 5 5 b b 5 5 b c . 
-. . c 5 5 5 b b b b 5 5 5 f . . 
-. . f f 5 5 5 5 5 5 5 5 f f . . 
-. . f f f b f e e f b f f f . . 
-. . f f f 1 f b b f 1 f f f . . 
-. . . f e e e b b e e e f . . . 
-. . . e b b e e e e b b e . . . 
-. . . e b b e b b e b b e . . . 
-. . . . c e e 5 5 e e c . . . . 
-. . . . f f f f f f f f . . . . 
-`];
-            case "witchPushLeft":
-            case "anim21":return [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . c c . . . . . . . 
-. . . . . . c c 5 c . . . . . . 
-. . . . c c 5 5 5 c c c . . . . 
-. . c c c c 5 5 5 5 c b c c . . 
-. c b b 5 b 5 5 5 5 b 5 b b c . 
-. c b 5 5 b b 5 5 b b 5 5 b c . 
-. . c 5 5 5 b b b b 5 5 5 f . . 
-. . . f 5 5 5 5 5 5 5 5 f f . . 
-. . . . f e e e f b e e f f . . 
-. . . . f e b b f 1 b f f f . . 
-. . . e e b b b b b b f f . . . 
-. . e b b e e e e e f . . . . . 
-. . e b b b b b 5 5 b f . . . . 
-. . . e e f 5 5 5 5 5 b f . . . 
-. . . . . f f f f f f f . . . . 
-`];
-            case "witchPushRight":
-            case "anim22":return [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . c c . . . . . . . 
-. . . . . . c 5 c c . . . . . . 
-. . . . c c c 5 5 5 c c . . . . 
-. . c c b c 5 5 5 5 c c c c . . 
-. c b b 5 b 5 5 5 5 b 5 b b c . 
-. c b 5 5 b b 5 5 b b 5 5 b c . 
-. . f 5 5 5 b b b b 5 5 5 c . . 
-. . f f 5 5 5 5 5 5 5 5 f . . . 
-. . f f e e b f e e e f . . . . 
-. . f f f b 1 f b b e f . . . . 
-. . . f f b b b b b b e e . . . 
-. . . . . f e e e e e b b e . . 
-. . . . f b 5 5 b b b b b e . . 
-. . . f b 5 5 5 5 5 f e e . . . 
-. . . . f f f f f f f . . . . . 
-`];
             case "witchPushBack":
             case "anim23":return [img`
 . . . . . . . . . . . . . . . . 
@@ -2204,6 +2147,63 @@ f . e e 1 1 1 a a 1 1 1 e e . f
 . . . e f 4 5 4 4 5 4 f e e . . 
 . . . . f 4 5 5 5 5 4 f . . . . 
 . . . . . f f f f f f . . . . . 
+`];
+            case "witchPushForward":
+            case "anim20":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . . f 5 f . . . . . . . 
+. . . . f f f 5 5 f f f . . . . 
+. . f f f f 5 5 5 5 f 4 f f . . 
+. f 4 4 5 4 5 5 5 5 4 5 4 4 f . 
+. f 4 5 5 4 4 5 5 4 4 5 5 4 f . 
+. . f 5 5 5 4 4 4 4 5 5 5 f . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. . f f f d f e e f d f f f . . 
+. . f f f 1 f a a f 1 f f f . . 
+. . . f e e e a a e e e f . . . 
+. . . e a a e e e e a a e . . . 
+. . . e a a e 4 4 e a a e . . . 
+. . . . f e e 5 5 e e f . . . . 
+. . . . f f f f f f f f . . . . 
+`];
+            case "witchPushLeft":
+            case "anim21":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f f 5 f . . . . . . 
+. . . . f f 5 5 5 f f f . . . . 
+. . f f f f 5 5 5 5 f 4 f f . . 
+. f 4 4 5 4 5 5 5 5 4 5 4 4 f . 
+. f 4 5 5 4 4 5 5 4 4 5 5 4 f . 
+. . f 5 5 5 4 4 4 4 5 5 5 f . . 
+. . . f 5 5 5 5 5 5 5 5 f f . . 
+. . . . f e e e f b e e f f . . 
+. . . . f e a a f 1 a f f f . . 
+. . . e e a a a a a a f f . . . 
+. . e a a e e e e e f . . . . . 
+. . e a a a a a 5 5 4 f . . . . 
+. . . e e f 5 5 5 5 5 4 f . . . 
+. . . . . f f f f f f f . . . . 
+`];
+            case "witchPushRight":
+            case "anim22":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f 5 f f . . . . . . 
+. . . . f f f 5 5 5 f f . . . . 
+. . f f 4 f 5 5 5 5 f f f f . . 
+. f 4 4 5 4 5 5 5 5 4 5 4 4 f . 
+. f 4 5 5 4 4 5 5 4 4 5 5 4 f . 
+. . f 5 5 5 4 4 4 4 5 5 5 f . . 
+. . f f 5 5 5 5 5 5 5 5 f . . . 
+. . f f e e b f e e e f . . . . 
+. . f f f a 1 f a a e f . . . . 
+. . . f f a a a a a a e e . . . 
+. . . . . f e e e e e a a e . . 
+. . . . f 4 5 5 a a a a a e . . 
+. . . f 4 5 5 5 5 5 f e e . . . 
+. . . . f f f f f f f . . . . . 
 `];
         }
         return null;
